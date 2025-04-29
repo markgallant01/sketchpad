cdde38211b3c335087dbc992f231a53cdc155f88
markgallant01 <markgallant01@gmail.com>
2025-04-29 17:11:12 -0400

    allow user to resize the sketchpad

--- javascript.js
+++ javascript.js
@@ -1,20 +1,39 @@
-const sketchpad = document.querySelector(".sketchpad");
-
-// create 16x16 pixel grid
-for (let i = 0; i < 16; i++) {
-  // create row
-  const row = document.createElement("div");
-  row.className = "row";
-  for (let j = 0; j < 16; j++) {
-    // create pixel
-    const pixel = document.createElement("div");
-    pixel.className = "pixel";
-    pixel.addEventListener("mouseover", (e) => fillBlack(e));
-    row.appendChild(pixel);
-  }
-  sketchpad.appendChild(row);
-}
-
 function fillBlack(event) {
   event.target.style.backgroundColor = "black";
 }
+
+function generateSketchpad(size) {
+  if (size > 100) { size = 100; }
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < size; j++) {
+      const pixel = document.createElement("div");
+      pixel.className = "pixel";
+      pixel.addEventListener("mouseover", (e) => fillBlack(e));
+      row.appendChild(pixel);
+    }
+    sketchpad.appendChild(row);
+  }
+}
+
+function resize() {
+  const input = window.prompt("Enter size: ");
+  const size = Number(input);
+
+  // wipe current sketchpad
+  while (sketchpad.firstChild) {
+    sketchpad.removeChild(sketchpad.firstChild);
+  }
+
+  generateSketchpad(size);
+}
+
+const sketchpad = document.querySelector(".sketchpad");
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => resize());
+
+// generate initial 16x16 sketchpad
+generateSketchpad(16);
